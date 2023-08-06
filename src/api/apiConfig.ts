48e9da345474580be5f08c676543edc6c0ccd684
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2023-08-06 05:15:16 -0300

    :wrench: Change jwt

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
   baseURL: 'http://127.0.0.1:3333/',
   headers: {
     Authorization:
-      'Bearer MQ._f3aUZHCOsZYQ_YzECs34gOW5dGOV_a_8PjHeoPrNDtJLFek4wPDjTMho3_Q',
+      'Bearer MQ.TTTkzjnA_YQA3-MMQFEhR9-Ms8ZUaEpZHl7VGAxJZihNsGyq_HNFRzjag3fB',
   },
 });
